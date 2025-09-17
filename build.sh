edc0e92cbc7203fd536cf95a525a24ef76394afb
Anurag Sarkar <angsak@Anurags-MacBook-Air.local>
2025-09-17 21:58:54 +0530

    fix

--- build.sh
+++ build.sh
@@ -3,4 +3,13 @@
 echo "Installing dependencies for all projects..."
 cd react-app
 npm install
+cd ..
+cd webpack-heavy-app
+npm install
+cd ..
+cd typescript-app
+npm install
+cd ..
+cd node-microservices
+npm install
 echo "All installations complete!"
