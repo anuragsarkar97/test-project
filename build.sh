5aa4e7214bd66dfc3dd5f7b86017f5ed73ce7760
Anurag Sarkar <angsak@Anurags-MacBook-Air.local>
2025-09-17 21:34:24 +0530

    fix

--- build.sh
+++ build.sh
@@ -2,13 +2,4 @@
 
 echo "Installing dependencies for all projects..."
 
-cd react-app && npm install && cd ..
-cd typescript-app && npm install && cd ..
-cd node-microservices && npm install && cd ..
-cd node-microservices/auth-service && npm install && cd ../..
-cd node-microservices/api-gateway && npm install && cd ../..
-cd node-microservices/worker-service && npm install && cd ../..
-cd webpack-heavy-app && npm install
-
-wait
 echo "All installations complete!"
